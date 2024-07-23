80487c87b686f8b34edf0d0d89aeea383e8a902e
Gohar-Elsaqq <gohar.elsaqqa@gmail.com>
2024-07-23 11:47:41 +0300

    add class CreditCardPayment

---
+++ src/main/java/org/example/CreditCardPayment.java
@@ -0,0 +1,8 @@
+package org.example;
+
+class CreditCardPayment extends Payment {
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Processing credit card payment of $" + amount);
+    }
+}
