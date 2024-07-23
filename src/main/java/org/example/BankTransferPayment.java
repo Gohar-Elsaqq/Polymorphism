52f099cef37ad9ce948707b67ea09caf449d3187
Gohar-Elsaqq <gohar.elsaqqa@gmail.com>
2024-07-23 11:52:45 +0300

    add class BankTransferPayment

---
+++ src/main/java/org/example/BankTransferPayment.java
@@ -0,0 +1,8 @@
+package org.example;
+
+class BankTransferPayment extends Payment {
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Processing bank transfer payment of $" + amount);
+    }
+}
